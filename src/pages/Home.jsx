93956264a02b4c4e9e1388fefa0fe4320a2f5e43
Ahmed-Imad <ahmedfattal@gmail.com>
2022-01-06 19:52:25 +0400

    before using reducers

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import UserResults from "../components/users/UserResults"
+function home() {
+    return (
+        <>
+            <UserResults/>            
+        </>
+    )
+}
+
+export default home
